df333de4fb554e0c3bc5cc17663d0575d4ed49fb
redviper2017 <tanzeerhossain@zoho.com>
2020-06-28 19:26:51 +0600

    Routes added to App.js, App.js code transfered to Home.js, SignIn page connected to TopNav Login button, React Player commented out and Iframe included in VideoPlayer.js & color changed in SignIn.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,16 @@
 import React from "react";
-import Carousel from "./components/Carousel";
-import "./App.css";
-import PrimarySearchAppBar from "./components/Topnav";
-import FAQView from "./components/FAQView";
-import { Grid, Paper, Divider, CardMedia, makeStyles } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/SignIn";
 
-import TopViewHome from "./components/TopViewHome";
-import InstructorViewHome from "./components/InstructorViewHome";
-import VideoPlayer from "./components/VideoPlayer";
-import Album from "./components/CourseViewHome";
-
-import Footer from "./components/Footer";
-
-const useStyles = makeStyles((theme) => ({
-  Divider: {
-    background: theme.palette.primary.light,
-    marginTop: theme.spacing(10),
-  },
-}));
-
-function App() {
-  const classes = useStyles();
-  return (
-    <div className="App">
-      <Grid container direction="column">
-        <Grid item>
-          <PrimarySearchAppBar />
-        </Grid>
-        <br />
-        <Grid item>
-          <TopViewHome />
-        </Grid>
-        <Grid item>
-          <Album />
-        </Grid>
-        <Grid container justify="center">
-          <Grid item lg={6}>
-            <VideoPlayer />
-          </Grid>
-        </Grid>
-        <Grid item>
-          <InstructorViewHome />
-        </Grid>
-        <Grid item>
-          <FAQView />
-        </Grid>
-        <Grid item>
-          <Carousel />
-        </Grid>
-        <Grid item className={classes.Divider}>
-          <Divider />
-        </Grid>
-        <Grid item>
-          <Footer />
-        </Grid>
-      </Grid>
-    </div>
+export default function App(){
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Redirect to="/"/>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Carousel from "./Carousel";
+import "../App.css";
+import PrimarySearchAppBar from "./Topnav";
+import FAQView from "./FAQView";
+import { Grid, Paper, Divider, CardMedia, makeStyles } from "@material-ui/core";
+
+import TopViewHome from "./TopViewHome";
+import InstructorViewHome from "./InstructorViewHome";
+import VideoPlayer from "./VideoPlayer";
+import Album from "./CourseViewHome";
+
+import Footer from "./Footer";
+
+const useStyles = makeStyles((theme) => ({
+  Divider: {
+    background: theme.palette.primary.light,
+    marginTop: theme.spacing(10),
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className="App">
+      <Grid container direction="column">
+        <Grid item>
+          <PrimarySearchAppBar />
+        </Grid>
+        <br />
+        <Grid item>
+          <TopViewHome />
+        </Grid>
+        <Grid item>
+          <Album />
+        </Grid>
+        <Grid container justify="center">
+          <Grid item lg={6}>
+            <VideoPlayer />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <InstructorViewHome />
+        </Grid>
+        <Grid item>
+          <FAQView />
+        </Grid>
+        <Grid item>
+          <Carousel />
+        </Grid>
+        <Grid item className={classes.Divider}>
+          <Divider />
+        </Grid>
+        <Grid item>
+          <Footer />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
