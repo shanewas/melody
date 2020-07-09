41b99757d0e644174721e64404d06edef2d0d1dc
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-07-09 20:10:28 +0600

    kinda normalized

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var coursesSchema = new Schema(
+var CourseSchema = new Schema(
 	{
 		title: { type: String, required: true },
 		subtitle: { type: String, required: true },
@@ -10,20 +10,44 @@ var coursesSchema = new Schema(
 		catagory: { type: String, required: true },
 		level: { type: Number, required: true },
 		sublevel: { type: Number, required: true },
-		videos: [{ type: String, required: true }],
-		instructor: { type: String, required: true },
-		preRequisites: [{ type: String }],
+		videos: [
+			{
+				type: Schema.Types.ObjectId,
+				required: true,
+				ref: "Video",
+			},
+		],
+		instructor: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: "Instructor",
+		},
+		preRequisites: [
+			{ type: Schema.Types.ObjectId, required: true, ref: "Course" },
+		],
 		requirements: { type: String },
-		nextCourses: [{ type: String }],
-		RelatedCourse: [{ type: String }],
+		nextCourses: [
+			{ type: Schema.Types.ObjectId, required: true, ref: "Course" },
+		],
+		RelatedCourse: [
+			{ type: Schema.Types.ObjectId, required: true, ref: "Course" },
+		],
 		courseHour: { type: Number, required: true },
-		document: [{ type: String }],
+		document: [
+			{ type: Schema.Types.ObjectId, required: true, ref: "Document" },
+		],
 		price: { type: Number, required: true },
 		validity: { type: Number, required: true },
-		studentReviews: [{ type: String }],
+		studentReviews: [
+			{
+				type: Schema.Types.ObjectId,
+				required: true,
+				ref: "StudentReviews",
+			},
+		],
 		certificate: { type: String, required: true },
 	},
 	{ timestamps: true }
 );
 
-module.exports = mongoose.model("Course", coursesSchema);
+module.exports = mongoose.model("Course", CourseSchema);
--- backend/routes/course.js
+++ backend/routes/course.js
@@ -9,42 +9,42 @@ router.route("/").get((req, res) => {
 
 router.route("/add").post((req, res) => {
 	const title = req.body.title;
-	const subtitle = req.body.subtitle;
-	const description = req.body.description;
-	const catagory = req.body.catagory;
-	const level = req.body.level;
-	const sublevel = req.body.sublevel;
-	const videos = req.body.videos;
+	// const subtitle = req.body.subtitle;
+	// const description = req.body.description;
+	// const catagory = req.body.catagory;
+	// const level = req.body.level;
+	// const sublevel = req.body.sublevel;
+	// const videos = req.body.videos;
 	const instructor = req.body.instructor;
-	const preRequisites = req.body.preRequisites;
-	const requirements = req.body.requirements;
-	const nextCourses = req.body.nextCourses;
-	const RelatedCourse = req.body.RelatedCourse;
-	const courseHour = req.body.courseHour;
-	const document = req.body.document;
-	const price = req.body.price;
-	const validity = req.body.validity;
-	const studentReviews = req.body.studentReviews;
-	const certificate = req.body.certificate;
+	// const preRequisites = req.body.preRequisites;
+	// const requirements = req.body.requirements;
+	// const nextCourses = req.body.nextCourses;
+	// const RelatedCourse = req.body.RelatedCourse;
+	// const courseHour = req.body.courseHour;
+	// const document = req.body.document;
+	// const price = req.body.price;
+	// const validity = req.body.validity;
+	// const studentReviews = req.body.studentReviews;
+	// const certificate = req.body.certificate;
 	const newCourse = new Course({
 		title,
-		subtitle,
-		description,
-		catagory,
-		level,
-		sublevel,
-		videos,
+		// subtitle,
+		// description,
+		// catagory,
+		// level,
+		// sublevel,
+		// videos,
 		instructor,
-		preRequisites,
-		requirements,
-		nextCourses,
-		RelatedCourse,
-		courseHour,
-		document,
-		price,
-		validity,
-		studentReviews,
-		certificate,
+		// preRequisites,
+		// requirements,
+		// nextCourses,
+		// RelatedCourse,
+		// courseHour,
+		// document,
+		// price,
+		// validity,
+		// studentReviews,
+		// certificate,
 	});
 	newCourse
 		.save()
