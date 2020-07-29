46d6d0678b563f0f0a1268b1780c8605b17cc041
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-07-29 20:50:11 +0600

    protected routes || weird menifest error removed || unknown endpoint to 404 page || config export module

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,11 +4,11 @@ import "../App.css";
 import Navbar from "./Navbar";
 import FAQView from "./FAQView";
 import {
-  Grid,
-  Divider,
-  makeStyles,
-  Typography,
-  FormControl,
+	Grid,
+	Divider,
+	makeStyles,
+	Typography,
+	FormControl,
 } from "@material-ui/core";
 
 import TopViewHome from "./TopViewHome";
@@ -24,193 +24,193 @@ import instance from "../api/Config";
 import ContactForm from "./ContactForm";
 
 const useStyles = makeStyles((theme) => ({
-  Divider: {
-    background: theme.palette.secondary.light,
-    marginTop: theme.spacing(10),
-  },
-  Divider2: {
-    background: theme.palette.secondary.light,
-    marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(10),
-  },
-  Carousel: {
-    color: theme.palette.primary.light,
-    marginTop: theme.spacing(8),
-  },
-  Typography: {
-    marginBottom: theme.spacing(4),
-    color: theme.palette.secondary.contrastText,
-  },
+	Divider: {
+		background: theme.palette.secondary.light,
+		marginTop: theme.spacing(10),
+	},
+	Divider2: {
+		background: theme.palette.secondary.light,
+		marginTop: theme.spacing(10),
+		marginBottom: theme.spacing(10),
+	},
+	Carousel: {
+		color: theme.palette.primary.light,
+		marginTop: theme.spacing(8),
+	},
+	Typography: {
+		marginBottom: theme.spacing(4),
+		color: theme.palette.secondary.contrastText,
+	},
 }));
 
 function App() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [video, setVideo] = useState("");
-  const [courseList, setCourseList] = useState([]);
-  const [instructorList, setInstructorList] = useState([]);
+	const [video, setVideo] = useState("");
+	const [courseList, setCourseList] = useState([]);
+	const [instructorList, setInstructorList] = useState([]);
 
-  const [res, setRes] = useState("");
+	const [res, setRes] = useState("");
 
-  useEffect(() => {
-    // login();
-    getIntroVideo();
-    getCourses();
-    getInstructors();
-  }, []);
+	useEffect(() => {
+		// login();
+		getIntroVideo();
+		getCourses();
+		getInstructors();
+	}, []);
 
-  //get the 2 videos in Home Page
-  function getIntroVideo() {
-    instance
-      .get("video/5f147d66d4c1340a1b1ff499/", {
-        headers: {
-          "auth-token": `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW5ld2FzYWhtZWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiJQb3RhdG83MjYiLCJpYXQiOjE1OTU4NjA3MzYsImV4cCI6MTU5NTg2NDMzNn0.IRPW-1hioz4LZABZrmtYakjmDwORfKnzIWkwK3DzAXc`,
-        },
-      })
-      .then((res) => {
-        const data = res.data;
+	//get the 2 videos in Home Page
+	function getIntroVideo() {
+		instance
+			.get("video/5f147d66d4c1340a1b1ff499/", {
+				headers: {
+					"auth-token": `${process.env.AUTH_SOMETHING}`,
+				},
+			})
+			.then((res) => {
+				const data = res.data;
 
-        setVideo(data.file);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+				setVideo(data.file);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  // //login
-  // function login() {
-  //   instance
-  //     .post("user/login/", {
-  //       email: "tanzeerhossain@zoho.com",
-  //       password: "123321",
-  //     })
-  //     .then((res) => {
-  //       console.log("login response: " + JSON.stringify(res.headers));
-  //       setRes(`${res.headers["set-cookie"]}`);
-  //       getCourses();
-  //     });
-  // }
+	// //login
+	// function login() {
+	//   instance
+	//     .post("user/login/", {
+	//       email: "tanzeerhossain@zoho.com",
+	//       password: "123321",
+	//     })
+	//     .then((res) => {
+	//       console.log("login response: " + JSON.stringify(res.headers));
+	//       setRes(`${res.headers["set-cookie"]}`);
+	//       getCourses();
+	//     });
+	// }
 
-  //get all courses from server later it will be list all featured courses
-  function getCourses() {
-    instance
-      .get("course/", {
-        headers: {
-          "auth-token": `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW5ld2FzYWhtZWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiJQb3RhdG83MjYiLCJpYXQiOjE1OTU4NjA3MzYsImV4cCI6MTU5NTg2NDMzNn0.IRPW-1hioz4LZABZrmtYakjmDwORfKnzIWkwK3DzAXc`,
-        },
-      })
-      .then((res) => {
-        const courseList = res.data;
-        setCourseList(courseList);
-        console.log("course list fetched in home: " + courseList);
-      });
-  }
+	//get all courses from server later it will be list all featured courses
+	function getCourses() {
+		instance
+			.get("course/", {
+				headers: {
+					"auth-token": `${process.env.AUTH_SOMETHING}`,
+				},
+			})
+			.then((res) => {
+				const courseList = res.data;
+				setCourseList(courseList);
+				console.log("course list fetched in home: " + courseList);
+			});
+	}
 
-  //get all instructor list from server later it will be list of all featured instructors
-  function getInstructors() {
-    instance
-      .get("instructor/", {
-        headers: {
-          "auth-token": `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW5ld2FzYWhtZWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiJQb3RhdG83MjYiLCJpYXQiOjE1OTU4NjA3MzYsImV4cCI6MTU5NTg2NDMzNn0.IRPW-1hioz4LZABZrmtYakjmDwORfKnzIWkwK3DzAXc`,
-        },
-      })
-      .then((res) => {
-        const instructorList = res.data;
-        setInstructorList(instructorList);
-        console.log("instructor list fetched in home: " + instructorList);
-      });
-  }
+	//get all instructor list from server later it will be list of all featured instructors
+	function getInstructors() {
+		instance
+			.get("instructor/", {
+				headers: {
+					"auth-token": `${process.env.AUTH_SOMETHING}`,
+				},
+			})
+			.then((res) => {
+				const instructorList = res.data;
+				setInstructorList(instructorList);
+				console.log("instructor list fetched in home: " + instructorList);
+			});
+	}
 
-  return (
-    <div className="App">
-      <Grid container direction="column" spacing={theme.spacing(1)}>
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <Navbar />
-        </Grid>
-        <br />
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <TopViewHome />
-        </Grid>
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <CourseViewHome courses={courseList} />
-        </Grid>
-        <Grid container direction="row">
-          <Grid item lg={2}></Grid>
-          <Grid item justify="center" lg={8} sm={12} md={12} xs={12}>
-            <VideoPlayer url={"http://162.0.231.67/" + video} />
-          </Grid>
-          <Grid item lg={2}></Grid>
-        </Grid>
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <InstructorViewHome instructors={instructorList} />
-        </Grid>
-      </Grid>
-      <Grid item className={classes.Divider2}>
-        <Divider />
-      </Grid>
-      <Grid item lg={12}>
-        <Typography variant="h4" className={classes.Typography}>
-          Meet our Instructors
-        </Typography>
-      </Grid>
-      <Grid container direction="column" spacing={theme.spacing(1)}>
-        <Grid container direction="row">
-          <Grid item lg={2}></Grid>
-          <Grid
-            item
-            lg={8}
-            sm={12}
-            md={12}
-            xs={12}
-            className={classes.Carousel}
-          >
-            <InstructorCarousel />
-          </Grid>
-          <Grid item lg={2}></Grid>
-        </Grid>
-        <Grid container direction="row">
-          <Grid item lg={2}></Grid>
-          <Grid item lg={8} sm={12} md={12} xs={12}>
-            <Info
-              numberOfCourse={courseList.length}
-              numberOfInstructors={instructorList.length}
-            />
-          </Grid>
-          <Grid item lg={2}></Grid>
-        </Grid>
-        <Grid item lg={12}>
-          <Typography variant="h4" className={classes.Typography}>
-            Our Student's Feedback
-          </Typography>
-        </Grid>
-        <Grid container direction="row">
-          <Grid item lg={4}></Grid>
-          <Grid item lg={4}>
-            <StudentFeedbackCarousel />
-          </Grid>
-          <Grid item lg={4}></Grid>
-        </Grid>
+	return (
+		<div className='App'>
+			<Grid container direction='column' spacing={theme.spacing(1)}>
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<Navbar />
+				</Grid>
+				<br />
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<TopViewHome />
+				</Grid>
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<CourseViewHome courses={courseList} />
+				</Grid>
+				<Grid container direction='row'>
+					<Grid item lg={2}></Grid>
+					<Grid item justify='center' lg={8} sm={12} md={12} xs={12}>
+						<VideoPlayer url={"http://162.0.231.67/" + video} />
+					</Grid>
+					<Grid item lg={2}></Grid>
+				</Grid>
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<InstructorViewHome instructors={instructorList} />
+				</Grid>
+			</Grid>
+			<Grid item className={classes.Divider2}>
+				<Divider />
+			</Grid>
+			<Grid item lg={12}>
+				<Typography variant='h4' className={classes.Typography}>
+					Meet our Instructors
+				</Typography>
+			</Grid>
+			<Grid container direction='column' spacing={theme.spacing(1)}>
+				<Grid container direction='row'>
+					<Grid item lg={2}></Grid>
+					<Grid
+						item
+						lg={8}
+						sm={12}
+						md={12}
+						xs={12}
+						className={classes.Carousel}
+					>
+						<InstructorCarousel />
+					</Grid>
+					<Grid item lg={2}></Grid>
+				</Grid>
+				<Grid container direction='row'>
+					<Grid item lg={2}></Grid>
+					<Grid item lg={8} sm={12} md={12} xs={12}>
+						<Info
+							numberOfCourse={courseList.length}
+							numberOfInstructors={instructorList.length}
+						/>
+					</Grid>
+					<Grid item lg={2}></Grid>
+				</Grid>
+				<Grid item lg={12}>
+					<Typography variant='h4' className={classes.Typography}>
+						Our Student's Feedback
+					</Typography>
+				</Grid>
+				<Grid container direction='row'>
+					<Grid item lg={4}></Grid>
+					<Grid item lg={4}>
+						<StudentFeedbackCarousel />
+					</Grid>
+					<Grid item lg={4}></Grid>
+				</Grid>
 
-        {/* <Grid item lg={12}>
+				{/* <Grid item lg={12}>
           <ContactForm />
         </Grid> */}
 
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <FAQView />
-        </Grid>
-        {/* end */}
-      </Grid>
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<FAQView />
+				</Grid>
+				{/* end */}
+			</Grid>
 
-      <Grid item className={classes.Divider}>
-        <Divider />
-      </Grid>
-      <Grid container direction="column" spacing={theme.spacing(1)}>
-        <Grid item lg={12} sm={12} md={12} xs={12}>
-          <Footer />
-        </Grid>
-      </Grid>
-    </div>
-  );
+			<Grid item className={classes.Divider}>
+				<Divider />
+			</Grid>
+			<Grid container direction='column' spacing={theme.spacing(1)}>
+				<Grid item lg={12} sm={12} md={12} xs={12}>
+					<Footer />
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -12,158 +12,164 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import auth from "../routes/auth";
 import "../theme";
 import theme from "../theme";
 import { useHistory } from "react-router-dom";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        MethodMelody
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+	return (
+		<Typography variant='body2' color='textSecondary' align='center'>
+			{"Copyright © "}
+			<Link color='inherit' href='https://material-ui.com/'>
+				MethodMelody
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    background: theme.palette.primary.light,
-    padding: theme.spacing(4, 5, 4, 5),
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.contrastText,
-    width: theme.spacing(10),
-    height: theme.spacing(10),
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    color: theme.palette.text.secondary,
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    background: theme.palette.secondary.contrastText,
-    color: theme.palette.primary.light,
-  },
-  label: {
-    "&:focus": {
-      color: theme.palette.text.secondary,
-    },
-    "&:active": {
-      color: theme.palette.text.secondary,
-    },
-  },
-  input: {
-    color: theme.palette.text.secondary,
-  },
+	root: {
+		background: theme.palette.primary.light,
+		padding: theme.spacing(4, 5, 4, 5),
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.contrastText,
+		width: theme.spacing(10),
+		height: theme.spacing(10),
+	},
+	form: {
+		width: "100%", // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+		color: theme.palette.text.secondary,
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+		background: theme.palette.secondary.contrastText,
+		color: theme.palette.primary.light,
+	},
+	label: {
+		"&:focus": {
+			color: theme.palette.text.secondary,
+		},
+		"&:active": {
+			color: theme.palette.text.secondary,
+		},
+	},
+	input: {
+		color: theme.palette.text.secondary,
+	},
 }));
 
 export default function SignIn() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const history = useHistory();
-  function navigateToSignup() {
-    history.push("/signup");
-  }
+	const history = useHistory();
+	function navigateToSignup() {
+		history.push("/signup");
+	}
 
-  return (
-    <Container component="main" maxWidth="xs" className={classes.root}>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography
-          component="h1"
-          variant="h5"
-          style={{ color: theme.palette.secondary.contrastText }}
-        >
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            className={classes.label}
-            InputProps={{
-              className: classes.input
-            }}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            InputProps={{
-              className: classes.input
-            }}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                value="remember"
-                style={{ color: theme.palette.secondary.contrastText }}
-              />
-            }
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link
-                href="#"
-                variant="body2"
-                style={{ color: theme.palette.secondary.contrastText }}
-              >
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link
-                href="#"
-                variant="body2"
-                style={{ color: theme.palette.secondary.contrastText }}
-                onClick={navigateToSignup}
-              >
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+	return (
+		<Container component='main' maxWidth='xs' className={classes.root}>
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography
+					component='h1'
+					variant='h5'
+					style={{ color: theme.palette.secondary.contrastText }}
+				>
+					Sign in
+				</Typography>
+				<form className={classes.form} noValidate>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						id='email'
+						label='Email Address'
+						name='email'
+						autoComplete='email'
+						className={classes.label}
+						InputProps={{
+							className: classes.input,
+						}}
+					/>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						name='password'
+						label='Password'
+						type='password'
+						id='password'
+						autoComplete='current-password'
+						InputProps={{
+							className: classes.input,
+						}}
+					/>
+					<FormControlLabel
+						control={
+							<Checkbox
+								value='remember'
+								style={{ color: theme.palette.secondary.contrastText }}
+							/>
+						}
+						label='Remember me'
+					/>
+					<Button
+						type='submit'
+						fullWidth
+						variant='contained'
+						color='primary'
+						className={classes.submit}
+						onClick={() => {
+							auth.login(() => {
+								history.push("/course");
+							});
+						}}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item xs>
+							<Link
+								href='#'
+								variant='body2'
+								style={{ color: theme.palette.secondary.contrastText }}
+							>
+								Forgot password?
+							</Link>
+						</Grid>
+						<Grid item>
+							<Link
+								href='#'
+								variant='body2'
+								style={{ color: theme.palette.secondary.contrastText }}
+								onClick={navigateToSignup}
+							>
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<Box mt={8}>
+				<Copyright />
+			</Box>
+		</Container>
+	);
 }
