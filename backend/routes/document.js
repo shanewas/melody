39914a99c3a06b10e263946b9c8b03776f0503e1
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-08-06 19:41:20 +0600

    accepting all type in document

--- backend/routes/document.js
+++ backend/routes/document.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 	},
 });
 const fileFilter = (req, file, cb) => {
-	if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+	if (true) {
 		cb(null, true);
 	} else {
 		cb(null, false);
