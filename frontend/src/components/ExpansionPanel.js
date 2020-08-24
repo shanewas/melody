f5e40c11c70d141b87ab3b0e9110ff77f374c704
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-08-24 12:08:06 +0600

    FAQ color changed

--- frontend/src/components/ExpansionPanel.js
+++ frontend/src/components/ExpansionPanel.js
@@ -1,32 +1,76 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import { makeStyles, withStyles } from "@material-ui/core";
+
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MuiExpansionPanel from "@material-ui/core/ExpansionPanel";
+import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import MuiExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import theme from "../theme";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",   
+    width: "100%",
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
-    background: "#191c21"
+    background: "#191c21",
   },
   heading: {
     fontSize: theme.typography.pxToRem(20),
     fontWeight: theme.typography.fontWeightRegular,
   },
   Typography: {
-    fontSize: theme.typography.pxToRem(16)
+    fontSize: theme.typography.pxToRem(16),
   },
   ExpandMoreIcon: {
-    color: "rgba(255,255,255,.87)"
+    color: "rgba(255,255,255,.87)",
   },
-  ExpansionPanel: {  
-    padding: theme.spacing(15)
-  }
+  ExpansionPanel: {
+    padding: theme.spacing(15),
+  },
 }));
+
+const ExpansionPanel = withStyles({
+  root: {
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+  },
+  expanded: {
+    margin: "auto",
+  },
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: theme.palette.primary.light,
+    borderBottom: "1px solid rgba(0,0,0,.125)",
+    marginBottom: -1,
+    minHeight: 56,
+
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})((props) => <MuiExpansionPanelSummary {...props} />);
+
+const ExpansionPanelDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing.unit * 2,
+    backgroundColor: theme.palette.primary.light,
+  },
+}))(MuiExpansionPanelDetails);
+
+ExpansionPanelSummary.muiName = "ExpansionPanelSummary";
 
 export default function SimpleExpansionPanel(props) {
   const classes = useStyles();
@@ -35,7 +79,7 @@ export default function SimpleExpansionPanel(props) {
     <div className={classes.root}>
       <ExpansionPanel square className={classes.root}>
         <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon className={classes.ExpandMoreIcon}/>}
+          expandIcon={<ExpandMoreIcon className={classes.ExpandMoreIcon} style={{color:theme.palette.secondary.contrastText}}/>}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
@@ -43,7 +87,7 @@ export default function SimpleExpansionPanel(props) {
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography align="left" className={classes.Typography}>
-          {props.answer}
+            {props.answer}
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
